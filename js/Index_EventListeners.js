3717ba2cbceb8d15e0fef82fb001b35aded47ca7
Juan Carlos Botero <jcarlosbotero1@outlook.com>
2022-12-27 07:44:51 -0500

    ...

--- js/Index_EventListeners.js
+++ js/Index_EventListeners.js
@@ -58,32 +58,4 @@ myForm.addEventListener('reset', (e) => {
 	}
 });
 
-myForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-	console.log('==========FORM HAS BEAN SUBMITED==========');
-	if (isloggedin()) {
-		let routeName = document.getElementById('fname').value;
-		let state = document.getElementById('state').value;
-		let address = [];
-		address[0] = document.getElementById('address1').value;
-		address[1] = document.getElementById('address2').value;
-		address[2] = document.getElementById('address3').value;
-		address[3] = document.getElementById('schoolAddress').value;
-		console.log(address);
 
-		document.getElementById('loadder').style.display = 'block';
-
-		let bid='M122201'
-		let active=true
-		let Monitor=false;
-
-		let url =
-			'https://script.google.com/macros/s/AKfycbwXtBXQJNEJF8veBWDU1Q3nu5C06rG9TyNqGP9JR1ZiPqUgRRRw3eY2lIKULHUXyJr2/exec';
-		url += `?state=${state}`;
-		// url += `&active=${active}`;
-		// url += `&bid=${bid}`;
-		// url += `&monitor=${Monitor}`;
-		makeAPICall(url);
-		// printResults((Drivers = testData()));
-	}
-});
