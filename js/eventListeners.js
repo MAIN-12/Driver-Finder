5d1a51abc34b81a61c8af26529a4209bbf3a949a
CHARLIE <jcarlosbotero1@outlook.com>
2023-01-05 00:25:06 -0500

    qwer

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -23,10 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	var elems = document.querySelectorAll('.collapsible');
 	var instances = M.Collapsible.init(elems);
 });
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.sidenav');
-//     var instances = M.Sidenav.init(elems);
-//   });
+
 
 
 addStudent.addEventListener('click', () => {
